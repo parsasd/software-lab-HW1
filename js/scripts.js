b7d6cb6b9e85843ce95b7a16e64a59f42f03358e
Parsa Sedighi <79263602+parsasd@users.noreply.github.com>
2024-10-19 01:04:44 +0330

    complete script.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
+// Function that display value
 function dis(val) {
-    document.getElementById("result").value += val;
+    document.getElementById("result").value += val
 }
 
 function initilize(event) {
@@ -17,8 +18,20 @@ var cal = document.getElementById("calcu");
 cal.onkeyup = function (event) {
     if (event.keyCode === 13) {
         console.log("Enter");
-        let x = document.getElementById("result").value;
+        let x = document.getElementById("result").value
         console.log(x);
         solve();
     }
+}
+
+// Function that evaluates the digit and return result
+function solve() {
+    let x = document.getElementById("result").value
+    let y = math.evaluate(x)
+    document.getElementById("result").value = y
+}
+
+// Function that clear the display
+function clr() {
+    document.getElementById("result").value = ""
 }
