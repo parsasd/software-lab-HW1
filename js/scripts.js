d984d0096ecc256494f6a54659faeaaa99d5eee3
Parsa Sedighi <79263602+parsasd@users.noreply.github.com>
2024-10-19 01:03:22 +0330

    add keyboard initialization for calculator inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('contact-form');
+function dis(val) {
+    document.getElementById("result").value += val;
+}
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        alert('Thank you for contacting us, ' + form.name.value + '!');
-
-        form.reset();
-    });
-});
+function initilize(event) {
+    if (event.key == '0' || event.key == '1' 
+        || event.key == '2' || event.key == '3'
+        || event.key == '4' || event.key == '5' 
+        || event.key == '6' || event.key == '7'
+        || event.key == '8' || event.key == '9' 
+        || event.key == '+' || event.key == '-'
+        || event.key == '*' || event.key == '/')
+        document.getElementById("result").value += event.key;
+}
